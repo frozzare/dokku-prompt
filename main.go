387208315e68cdf214c00c957da8e4a9e65bb227
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2017-09-13 13:46:39 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/c-bata/go-prompt"
+)
+
+var (
+	version  string
+	revision string
+)
+
+// Executor handle input.
+func Executor(h, c string) func(string) {
+	return func(s string) {
+		s = strings.TrimSpace(s)
+		if s == "" {
+			return
+		} else if s == "quit" || s == "exit" {
+			fmt.Println("Bye!")
+			os.Exit(0)
+			return
+		}
+
+		c = strings.TrimSpace(strings.Join([]string{c, s}, " "))
+		c = fmt.Sprintf("ssh %s '%s'", h, c)
+
+		cmd := exec.Command("/bin/sh", "-c", c)
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			fmt.Printf("Got error: %s\n", err.Error())
+		}
+	}
+}
+
+// Completer returns prompt suggestions.
+func Completer(d prompt.Document) []prompt.Suggest {
+	return []prompt.Suggest{}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("No dokku host provided")
+	}
+
+	host := os.Args[1]
+	cmd := ""
+	if len(os.Args) > 2 {
+		cmd = os.Args[2]
+	}
+
+	fmt.Printf("dokku-prompt %s (rev-%s)\n", version, revision)
+	fmt.Println("Please use `exit` or `Ctrl-D` to exit this program.")
+
+	defer fmt.Println("Bye!")
+
+	p := prompt.New(
+		Executor(host, cmd),
+		Completer,
+		prompt.OptionTitle("dokku: interactive dokku client"),
+		prompt.OptionPrefix(">>> "),
+		prompt.OptionInputTextColor(prompt.Yellow),
+	)
+	p.Run()
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# dokku-prompt
+
+Simple dokku prompt (no suggestion exists).
+
+## Installation
+
+```
+go get -u github.com/frozzare/dokku-prompt
+```
+
+# License
+
+MIT © [Fredrik Forsmo](https://github.com/frozzare)
